e02b35e0e9b7b5af05ae3a23f97256dd30597e10
Josh Sommer <joshdsommer@gmail.com>
2021-10-18 14:41:42 -0400

    script adjustments

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,7 +3,7 @@ cd apps/web-ngrx-chess/src/environments
 
 echo Writing secrets
 
-sed -i '' -e 's/SUPABASE_URL/'"$SUPABASE_URL"'/' environment.ts
-sed -i '' -e 's/SUPABASE_KEY/'"$SUPABASE_KEY"'/' environment.ts
+sed -i 's/SUPABASE_URL/'"$SUPABASE_URL"'/' environment.ts
+sed -i 's/SUPABASE_KEY/'"$SUPABASE_KEY"'/' environment.ts
 
 echo Done writing secrets
