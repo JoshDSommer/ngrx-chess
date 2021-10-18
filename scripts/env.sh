75f138832df2ce43f9bff12475754f510781557d
Josh Sommer <joshdsommer@gmail.com>
2021-10-18 15:35:01 -0400

    script cleanup

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,7 +3,11 @@ cd apps/web-ngrx-chess/src/environments
 
 echo Writing secrets
 
-sed -i 's/SUPABASE_URL/'"$SUPABASE_URL"'/' environment.ts
-sed -i 's/SUPABASE_KEY/'"$SUPABASE_KEY"'/' environment.ts
+SEDOPTION="-i"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed -i '' -e 's/SUPABASE_URL/'"$SUPABASE_URL"'/;s/SUPABASE_KEY/'"$SUPABASE_KEY"'/' environment.ts
+else
+	sed -i 's/SUPABASE_URL/'"$SUPABASE_URL"'/;s/SUPABASE_KEY/'"$SUPABASE_KEY"'/' environment.ts
+fi
 
 echo Done writing secrets
