563ad4166ca83d1894d932c38f2c9078572131e5
Josh Sommer <joshdsommer@gmail.com>
2022-06-25 10:30:45 -0400

    adding second application

--- apps/second-app/src/environments/environment.ts
+++ apps/second-app/src/environments/environment.ts
@@ -4,9 +4,6 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: 'https://jtjghyhcimlibcmmmetv.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNjUzNzYwNSwiZXhwIjoxOTQyMTEzNjA1fQ.3azIU6aELHDLhE_JQMMP_rSwBmFdCl7YW8FA15JzxaM',
 };
 
 /*
--- apps/web-ngrx-chess/src/environments/environment.prod.ts
+++ apps/web-ngrx-chess/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://SUPABASE_URL',
-  supabaseKey: 'SUPABASE_KEY',
+  supabaseUrl: 'https://jtjghyhcimlibcmmmetv.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNjUzNzYwNSwiZXhwIjoxOTQyMTEzNjA1fQ.3azIU6aELHDLhE_JQMMP_rSwBmFdCl7YW8FA15JzxaM',
 };
