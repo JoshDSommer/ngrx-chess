5298d9237fe8cea5c89be6d9fcea654d35c35938
Josh Sommer <joshdsommer@gmail.com>
2022-06-25 23:04:08 -0400

    removing broken build stuff

--- apps/web-ngrx-chess/src/app/app.routes.ts
+++ apps/web-ngrx-chess/src/app/app.routes.ts
@@ -2,20 +2,6 @@ import { Routes } from '@angular/router';
 
 export const appRoutes = [
   {
-    path: 'game',
-    loadChildren: () =>
-      import('../../../../libs/game/src/lib/game.module').then(
-        (m) => m.GameModule
-      ),
-  },
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('../../../../libs/login/src/lib/login.module').then(
-        (m) => m.LoginModule
-      ),
-  },
-  {
     path: 'logout',
     redirectTo: 'login',
   },
